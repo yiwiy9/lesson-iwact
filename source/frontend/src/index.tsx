ed9e9c4e1a75159fc2a15d097b2a47e76cebe76d
yiwiy9 <yiwiy9@gmail.com>
2022-03-17 16:56:25 +0900

    Change to Functional Component

--- source/frontend/src/index.tsx
+++ source/frontend/src/index.tsx
@@ -1,4 +1,11 @@
-import React from "react";
+/**
+ * 参考
+ * https://ja.reactjs.org/tutorial/tutorial.html
+ * https://nishinatoshiharu.com/ts-react-tutorial/
+ * https://zenn.dev/roiban/articles/473f9cbf2b793a
+ */
+
+import { useState } from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 
@@ -54,84 +61,80 @@ type HistoryData = {
 }
 
 type GameState = {
-  history: Array<HistoryData>;
-  stepNumber: number;
-  xIsNext: boolean;
+  readonly history: Array<HistoryData>;
+  readonly stepNumber: number;
+  readonly xIsNext: boolean;
 }
 
-class Game extends React.Component<{}, GameState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
-      stepNumber: 0,
-      xIsNext: true,
-    };
-  }
+const Game = () => {
+  const [state, setState] = useState<GameState>({
+    history: [{
+      squares: Array(9).fill(null),
+    }],
+    stepNumber: 0,
+    xIsNext: true,
+  });
 
-  handleClick(i: number) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+  const handleClick = (i: number) => {
+    const history = state.history.slice(0, state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
+    squares[i] = state.xIsNext ? 'X' : 'O';
+    setState({
       history: history.concat([{ // push()ではなく、ミューテートしないconcat()を使う
         squares: squares,
       }]),
       stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
+      xIsNext: !state.xIsNext,
     });
   }
 
-  jumpTo(stepNumber: number) {
-    this.setState({
+  const jumpTo = (stepNumber: number) => {
+    setState(prev => ({
+      ...prev,
       stepNumber: stepNumber,
       xIsNext: stepNumber%2 === 0,
-    })
+    }))
   }
 
-  render() {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const winner = calculateWinner(current.squares);
-    let status: string;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
+  const history = state.history.slice(0, state.stepNumber + 1);
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+  let status: string;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (state.xIsNext ? 'X' : 'O');
+  }
 
-    const moves = history.map((step, move) => {
-      const desc = move===0 ?
-        'Go to game start' :
-        'Go to move #' + move;
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
+  const moves = history.map((step, move) => {
+    const desc = move===0 ?
+      'Go to game start' :
+      'Go to move #' + move;
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
     );
-  }
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          squares={current.squares}
+          onClick={(i) => handleClick(i)}
+        />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
 }
 
 const calculateWinner = (squares: Array<SquareType>) => {
